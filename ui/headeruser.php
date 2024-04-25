df07670fe3ba3c49d99545ab0fab470278da2180
Edrian C. Cabarrubias <142069695+ian0820@users.noreply.github.com>
2024-04-25 22:27:53 +0800

    Update headeruser.php

--- ui/headeruser.php
+++ ui/headeruser.php
@@ -15,6 +15,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="../dist/css/adminlte.min.css">
+  
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
