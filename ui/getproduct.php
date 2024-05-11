79d0dea4a7e111558b9c4b9f6b20868afad126b0
Edrian C. Cabarrubias <142069695+ian0820@users.noreply.github.com>
2024-05-12 00:00:44 +0800

    Update getproduct.php

--- ui/getproduct.php
+++ ui/getproduct.php
@@ -7,7 +7,7 @@ $productid= $_GET["id"];
 
 $barcode=$_GET["id"];
 
-$select=$pdo->prepare("select * from tbl_product where pID=$productid OR barcode=$barcode");
+$select=$pdo->prepare("select * from tbl_product where pID=$productid OR Barcode=$barcode");
 $select->execute();
 
 $row=$select->fetch(PDO::FETCH_ASSOC);
